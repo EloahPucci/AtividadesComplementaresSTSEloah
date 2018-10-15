4cccf641918f25f47bbfc341fcced1ecd3940c95
Eloah Pucci <eloahpucci@gmail.com>
2018-10-15 00:43:40 -0300

    Adicionando casos de teste

--- src/test/java/br/edu/iftm/atividadeComplementar/domains/AtividadeTest.java
+++ src/test/java/br/edu/iftm/atividadeComplementar/domains/AtividadeTest.java
@@ -28,6 +28,39 @@ public class AtividadeTest {
 	}
 	
 	@Test
+	public void testaDisciplinasExtraCurriculares() {
+		int percentualCurso = 40;
+		int quantidadeHoras = 200;
+		atividade.setNome("Cursos e Minicursos");
+		atividade.setMaximoAtividadesSemestre(1);
+		atividade.setPercentualPorAtividade(percentualCurso);
+		int qtdHoras = atividade.getValorLimiteHorasAtividade(quantidadeHoras);
+		Assert.assertEquals(qtdHoras,80);
+	}
+	
+	@Test
+	public void testaParticipacaoColegiadosConselhos() {
+		int percentualCurso = 10;
+		int quantidadeHoras = 100;
+		atividade.setNome("Participação em Colegiados/Conselhos");
+		atividade.setMaximoAtividadesSemestre(1);
+		atividade.setPercentualPorAtividade(percentualCurso);
+		int qtdHoras = atividade.getValorLimiteHorasAtividade(quantidadeHoras);
+		Assert.assertEquals(qtdHoras,10);
+	}
+	
+	@Test
+	public void testaIntegranteGrupoEstudo() {
+		int percentualCurso = 20;
+		int quantidadeHoras = 10;
+		atividade.setNome("Integrante de Núcleo ou Grupo de Estudo");
+		atividade.setMaximoAtividadesSemestre(3);
+		atividade.setPercentualPorAtividade(percentualCurso);
+		int qtdHoras = atividade.getValorLimiteHorasAtividade(quantidadeHoras);
+		Assert.assertEquals(qtdHoras,6);
+	}
+	
+	@Test
 	public void testaQuantidadeLimiteHorasLicenciatura() {
 		int percentualCurso = 40;
 		int quantidadeHorasLicenciatura = 200;
